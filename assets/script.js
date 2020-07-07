46489ffc361969dfd270ce55851b46fb73a8560c
Jared Schau <jaredschau@MacBook-Pro.local>
2020-07-06 21:00:39 -0600

    got color changing working still need to figure out local save

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,46 @@
 //moment import var
-const m = moment();
+// format we need for this app
+// var m = moment();
+var currentDay = $("#currentDay").text(moment().format("dddd, MMMM do"))
+moment().format("MMMM Do YYYY, h:mm:ss a ");
+
+var date = new Date();
+var hours = date.getHours();
+
 
 // var for save button
 var text;
 var hourI;
 // first test making sure moment is workin
-console.log(m.format("dddd, MMMM Do"))
+// console.log(m.format("dddd, MMMM Do"))
 
-// format we need for this app
-$("#currentDay").text(m.format("dddd, MMMM do"))
+
 
 // inital functions
 // need to change color based on what time it is
 // need to save to local storage
 // need to autopdate using moment
 
-$(document).ready( function(){
-    colorShift
-    changeText
-});
+// $(document).ready( function(){
+//     colorShift
+//     changeText
+// });
 
-function colorShift (){
-    //defining current time
-    var realTime = moment().hours();
-    console.log("Current time" + realTime);
-// need to do for loop for past and future
+// function colorShift (){
+//     //defining current time
+//     var realTime = moment().hours();
+//     console.log("Current time" + realTime);
+
+// // need to do for loop for past and future
+// cant figure out that coude need to to if loop
+
+$(".time-block").each(function(){
+    var currentTime = $(this).attr("data-time")
+    if (hours > currentTime){
+        $(this).css("background-color", "yellow");
+    } else if (hours < currentTime){
+        $(this).css("background-color", "green");
+    } else {
+        $(this).css("background-color", "white")
+    }
+});
